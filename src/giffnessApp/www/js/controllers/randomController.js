dce23575e4389fc37a8ebafc5ef8ca30783931e6
shauntir <shauntir@gmail.com>
2015-09-29 20:34:36 +0200

    Add ngcordova and share function on random controller.

--- src/giffnessApp/www/js/controllers/randomController.js
+++ src/giffnessApp/www/js/controllers/randomController.js
@@ -1,9 +1,9 @@
 (function() {
   'use strict';
 
-  angular.module('giffnessApp').controller('RandomController', ['$scope', 'giphyApiService', 'giphyApiPrettifyService', randomController]);
+  angular.module('giffnessApp').controller('RandomController', ['$scope', 'giphyApiService', 'giphyApiPrettifyService', '$cordovaSocialSharing', randomController]);
 
-  function randomController($scope, giphyApiService, giphyApiPrettifyService) {
+  function randomController($scope, giphyApiService, giphyApiPrettifyService, $cordovaSocialSharing) {
     var viewModel = this;
 
     var requestRandomGif = function() {
@@ -14,9 +14,20 @@
         });
     };
 
-
     viewModel.getAnotherRandomGif = function() {
       requestRandomGif();
+    };
+
+    viewModel.shareGif = function(randomGif) {
+      ionic.Platform.ready(function() {
+        $cordovaSocialSharing
+            .share(null, "Hi! I just wanted to share this awesome gif with you.", randomGif.imageUrl, null)
+            .then(function(result) {
+              // Move along, nothing to see here.
+            }, function(err) {
+              // An error occured. Show a message to the user
+            });
+      });
     };
 
     requestRandomGif();
