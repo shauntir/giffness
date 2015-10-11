99c1e20706d400379d60377fe7ba292a1c96438e
shauntir <shauntir@gmail.com>
2015-10-11 12:34:45 +0200

    Trying to get download feature to work.

--- src/giffnessApp/www/js/controllers/trendingController.js
+++ src/giffnessApp/www/js/controllers/trendingController.js
@@ -1,9 +1,9 @@
 (function() {
   'use strict';
 
-  angular.module('giffnessApp').controller('TrendingController', ['$state', '$scope', '$ionicModal', 'giphyApiService', 'giphyApiPrettifyService', 'utilitiesHelperService', trendingController]);
+  angular.module('giffnessApp').controller('TrendingController', ['$window', '$state', '$scope', '$ionicModal', 'giphyApiService', 'giphyApiPrettifyService', 'utilitiesHelperService', '$cordovaFileTransfer', '$cordovaFile', '$ionicLoading', trendingController]);
 
-  function trendingController($state, $scope, $ionicModal, giphyApiService, giphyApiPrettifyService, utilitiesHelperService) {
+  function trendingController($window, $state, $scope, $ionicModal, giphyApiService, giphyApiPrettifyService, utilitiesHelperService, $cordovaFileTransfer, $cordovaFile, $ionicLoading) {
     var viewModel = this;
     var limit = 5;
     var offset = 0;
@@ -29,6 +29,81 @@
       utilitiesHelperService.shareGif(randomGif);
     };
 
+/*
+    viewModel.downloadGif = function(randomGif) {
+
+      ionic.Platform.ready(function() {
+        var url = randomGif.fixedWidthStillUrl;
+        var targetPath = LocalFileSystem.PERSISTENT + randomGif.title + ".gif";
+        var trustHosts = true
+        var options = {};
+        alert(targetPath);
+
+        $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+          .then(function(result) {
+                  alert('done: ' + targetPath);
+          }, function(err) {
+            // Error
+                  alert('error');
+          }, function (progress) {
+            //$timeout(function () {
+            //  $scope.downloadProgress = (progress.loaded / progress.total) * 100;
+            //})
+          });
+      });
+alert('fin');
+    };
+    */
+/*
+    viewModel.downloadGif = function(randomGif) {
+      ionic.Platform.ready(function() {
+
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
+            fs.root.getDirectory(
+                "ExampleProject",
+                {
+                    create: true
+                },
+                function(dirEntry) {
+                    dirEntry.getFile(
+                        "test.png",
+                        {
+                            create: true,
+                            exclusive: false
+                        },
+                        function gotFileEntry(fe) {
+                            var p = fe.toURL();
+                            fe.remove();
+                            ft = new FileTransfer();
+                            ft.download(
+                                encodeURI("http://ionicframework.com/img/ionic-logo-blog.png"),
+                                p,
+                                function(entry) {
+                                    $ionicLoading.hide();
+                                    $scope.imgFile = entry.toURL();
+                                },
+                                function(error) {
+                                    $ionicLoading.hide();
+                                    alert("Download Error Source " + error.source);
+                                },
+                                false,
+                                null
+                            );
+                        },
+                        function() {
+                            alert('error file');
+                        }
+                    );
+                }
+            );
+        },
+        function() {
+
+          alert('errpr');
+        });
+      });
+    };
+*/
     $scope.showGifModal = function(item) {
       $scope.modal.gif = item;
       $scope.modal.show();
